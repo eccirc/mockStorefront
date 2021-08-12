c4fc6307f04410294bd36fa7096f7696a4923a38
David <davidgw@protonmail.com>
2021-08-12 14:48:57 +0100

    add cart sorting logic

--- src/pages/CartContent/CartContent.tsx
+++ src/pages/CartContent/CartContent.tsx
@@ -1,9 +1,29 @@
 import React, { useContext } from 'react';
-import MasterStore from '../../components/storeApi/allProductsStore';
+import ProductStoreState from '../../components/storeApi/storeState';
+import { Product } from '../../types/Product';
+
+export interface sortedCart {
+    item: Product;
+    quantity: number;
+}
 
 const Cart: React.FC = () => {
-    const store = useContext(MasterStore);
+    const store = useContext(ProductStoreState);
     const { cart } = store;
+
+    const countOccurrences = (arr: Product[], val: Product) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+    const sortCart = () => {
+        const Cart: sortedCart[] = [];
+        const uniqueItems = Array.from(new Set(cart));
+        uniqueItems.forEach((product) => {
+            Cart.push({ item: product, quantity: countOccurrences(cart, product) });
+        });
+        return Cart;
+    };
+    const sorted = sortCart();
+
+    console.log(sorted);
 
     return (
         <div className="w-full h-screen heading mx-auto bg-white grid grid-cols-1 md:grid-cols-2">
@@ -15,7 +35,7 @@ const Cart: React.FC = () => {
                 <h2 className="text-3xl font-bold mb-4">Checkout Now</h2>
                 <span className="flex justify-between text-2xl">
                     <h3>Total</h3>
-                    <h3>£0.00</h3>
+                    <h3>£{store.cartTotal > 0 ? store.cartTotal : 0}</h3>
                 </span>
             </div>
         </div>
